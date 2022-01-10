12b97bb1c3c445519b818baef0f88b5de0fc9812
Ren Wang <iamwrm@gmail.com>
2022-01-10 17:59:59 +0800

    fix conda path

--- quant_env/download_conda.sh
+++ quant_env/download_conda.sh
@@ -7,4 +7,4 @@ cd /root
 wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.10.3-Linux-x86_64.sh
 bash Miniconda3-py37_4.10.3-Linux-x86_64.sh -b -p "/root/miniconda"
 
-CONDA_PATH="/root/miniconda/bin"
+CONDA_PATH="/root/miniconda"
