3d9029404abea3a725a8bfd75ff5c54bad3ac87b
Ren Wang <iamwrm@gmail.com>
2022-01-10 14:58:16 +0000

    dont use sh

--- quant_env/conda_common_0.sh
+++
@@ -1,9 +0,0 @@
-cat /tmp/dockerbuild/conda_path.txt
-CONDA_PATH=$(cat /tmp/dockerbuild/conda_path.txt)
-CONDA=${CONDA_PATH}/bin/conda
-
-${CONDA} create -q -n py37env python=3.7.6
-${CONDA} init bash
-
-bash ${CONDA_PATH}/etc/profile.d/conda.sh 
-
